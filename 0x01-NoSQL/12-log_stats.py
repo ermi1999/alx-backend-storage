662f9eff58fbc93bdf3ee3b5fd5e4203ac90eb02
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-14 06:33:01 -0700

    status checks

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,21 +5,23 @@ finds some log status.
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-collection = client.logs.nginx
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
 
-logs = collection.estimated_document_count()
-methods = {
+    logs = collection.estimated_document_count()
+    methods = {
         "GET": collection.count_documents({"method": "GET"}),
         "POST": collection.count_documents({"method": "POST"}),
         "PUT": collection.count_documents({"method": "PUT"}),
         "PATCH": collection.count_documents({"method": "PATCH"}),
         "DELETE": collection.count_documents({"method": "DELETE"})
     }
-status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    status_checks = collection.count_documents(
+            {"method": "GET", "path": "/status"})
 
-print("{} logs".format(logs))
-print("Methods:")
-for key, value in methods.items():
-    print("\tmethod {}: {}".format(key, value))
-print("{} status check".format(status_check))
+    print("{} logs".format(logs))
+    print("Methods:")
+    for key, value in methods.items():
+        print("\tmethod {}: {}".format(key, value))
+    print("{} status check".format(status_checks))
