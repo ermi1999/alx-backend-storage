c07dec0bd8974c4542f08ae199d905a8bbdc385e
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-15 17:18:38 +0300

    implemented an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python3
 """
-Caching request module
+module for requesting a page.
 """
-import redis
 import requests
+import redis
 from functools import wraps
 from typing import Callable
 
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
+r = redis.Redis()
+
+
+def cache_response(fun: Callable) -> Callable:
     """
-    @wraps(fn)
+    A decorator for caching a response and for counting a request.
+    """
+    @wraps(fun)
     def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
+        r.incr(f'count:{url}')
+        cached = r.get(f'result:{url}')
+        if cached:
+            return cached.decode('utf-8')
+        res = fun(url)
+        r.setex(f'result:{url}', 10, res)
+        return res
     return wrapper
 
 
-@track_get_page
+@cache_response
 def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
     """
-    response = requests.get(url)
-    return response.text
+    makes a reques to url.
+    """
+    return requests.get(url).text
