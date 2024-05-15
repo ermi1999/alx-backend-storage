fe827687eaeb67ce2104b10e507fe59bdd856ae0
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-15 12:29:37 +0300

    store a string

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
     def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     def store(self, data: str | bytes | int | float) -> str:
         """
         stores a data.
